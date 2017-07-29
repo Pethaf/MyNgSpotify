1d64220088343bc5ec7d847f8560776432d05486
Peter Hafid <peter@ctrl-c.liu.se>
2017-07-29 20:07:31 +0200

    Made navbar collapsible using property binding

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,5 +4,13 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'navbar',
   templateUrl: `navbar.component.html`,
+  styleUrls: ["navbar.component.css"]
 })
-export class NavbarComponent  {  }
+export class NavbarComponent  { 
+    show:boolean = false;
+    onClick(): void
+    {
+        this.show = !this.show;
+        console.log(this.show);
+    }
+ }
