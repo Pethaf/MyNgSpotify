97b3a122edc2b819a9783dde1987cd86edbb3818
Peter Hafid <peter@ctrl-c.liu.se>
2017-07-29 20:18:02 +0200

    Start work on serchMusic functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AboutComponent } from './components/about/about.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { routing } from "./components/app.routing";
+import { FormsModule} from "@angular/forms";
 
 @NgModule({
-  imports:      [ BrowserModule, routing ],
+  imports:      [ BrowserModule, routing, FormsModule ],
   declarations: [ AppComponent,
                   SearchComponent,
                   AboutComponent, 
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
     moduleId:module.id,
     selector: 'search',
     templateUrl: 'search.component.html'
 })
-export class SearchComponent {}
+export class SearchComponent {
+    searchStr:string;
+    searchMusic():void 
+    {
+        console.log(this.searchStr);
+    }
+}
